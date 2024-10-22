4d827e71e833ebfc297c7e2ec35d62c77dd23f18
JoyceMolenaar0 <146432917+JoyceMolenaar0@users.noreply.github.com>
2024-10-23 00:35:22 +0200

    BuyingTowersDone

--- TowerDefence/Assets/Scripts/Towers/TowerEnabler/BuyTowers.cs
+++ TowerDefence/Assets/Scripts/Towers/TowerEnabler/BuyTowers.cs
@@ -5,16 +5,47 @@ using UnityEngine;
 
 public class BuyTowers : MonoBehaviour
 {
-    
+    [SerializeField] GameObject[] Towers;
+    [SerializeField] GameObject[] BulletCloneScript;
+    [SerializeField] int[] TowerCost;
+
+    private CoinsUpdater CoinsUpdateScript;
+
     void Start()
     {
-        
+        GameObject CoinsUpdaterObject = GameObject.FindWithTag("CoinsUpdater");
+        if (CoinsUpdaterObject != null)
+        {
+            CoinsUpdateScript = CoinsUpdaterObject.GetComponent<CoinsUpdater>();
+        }
+
+       for (int i = 0; i < Towers.Length; i++)
+        {
+            Towers[i].SetActive(false);
+            BulletCloneScript[i].SetActive(false);
+        }
+
+         
     }
 
    
     void Update()
     {
         
+    }
+
+    public void BuyTheTowers(int TowerNumber)
+    {
+        if (TowerNumber >= 0 && TowerNumber < Towers.Length)
+        {
+            if (CoinsUpdateScript.PlayerCoins >= TowerCost[TowerNumber])
+            {
+                Towers[TowerNumber].SetActive(true);
+                BulletCloneScript[TowerNumber].SetActive(true);
+                CoinsUpdateScript.PlayerCoins -= TowerCost[TowerNumber];
+                CoinsUpdateScript.UpdateCoins();
+            }
+        }
     }
 
     
