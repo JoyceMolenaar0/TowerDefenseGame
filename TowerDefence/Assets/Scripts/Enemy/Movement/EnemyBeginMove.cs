7030d992fc9e432eb22f7cfd4d9ba63142a5b4ac
JoyceMolenaar0 <146432917+JoyceMolenaar0@users.noreply.github.com>
2024-10-22 10:18:46 +0200

    EnemyWalkFixAnimation

--- TowerDefence/Assets/Scripts/Enemy/Movement/EnemyBeginMove.cs
+++ TowerDefence/Assets/Scripts/Enemy/Movement/EnemyBeginMove.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemyBeginMove : MonoBehaviour
 {
     [SerializeField] Animator EnemyAnimator;
+    [SerializeField] GameObject DoorPoint;
+    
     void Start()
     {
-        EnemyAnimator.SetBool("EnemyKick", true);
+        EnemyAnimator.SetBool("IsWalking", true);
     }
 
    
     void Update()
     {
-        
+        if (Vector3.Distance(transform.position, DoorPoint.transform.position) < 0.2f)
+        {
+            IsKicking();
+        }
+        else
+        {
+            IsWalking();
+        }
+    }
+
+    void IsWalking()
+    {
+        EnemyAnimator.SetBool("IsWalking", true);
+        EnemyAnimator.SetBool("IsDying", false);
+        EnemyAnimator.SetBool("IsKicking", false);
+    }
+
+    void IsDying()
+    {
+        EnemyAnimator.SetBool("IsDying", true);
+        EnemyAnimator.SetBool("IsWalking", false);
+        EnemyAnimator.SetBool("IsKicking", false);
+    }
+
+    void IsKicking()
+    {
+        EnemyAnimator.SetBool("IsKicking", true);
+        EnemyAnimator.SetBool("IsWalking", false);
+        EnemyAnimator.SetBool("IsDying", false);
     }
 }
